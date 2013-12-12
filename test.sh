1a117e05fc13ebfb6d04d82006ebda62eee86d64
benpop <thebassplayer@gmail.com>
2013-12-11 21:40:11 -0800

    hand off arguments to test program to process terminal/screen/"paper" size for table formatting

--- test.sh
+++ test.sh
@@ -6,4 +6,4 @@ sed -e 's/^/a /' /usr/share/dict/words > "$TMP"
 
 make test
 
-cat "$TMP" test.txt | ./test 2>&1 | sed -e '/^ok$/d' > test.out
+cat "$TMP" test.txt | ./test "$@" 2>&1 | sed -e '/^ok$/d' > test.out
